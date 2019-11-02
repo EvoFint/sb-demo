7c21fe0e9802fbfeee4a314d9fd7e9691ae05a8a
evofint <evofint@gmail.com>
2019-11-03 01:42:50 +0700

    Remake ViewEmp for empty store

--- src/components/ViewEmp.js
+++ src/components/ViewEmp.js
@@ -1,70 +1,75 @@
-import React, { Component } from 'react';
+import React, {  } from 'react';
 import { connect } from "react-redux";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import Moment from 'moment';
 
-class ViewEmp extends Component {
-    constructor(props) {
-        super(props);
-        this.state = ({
-            employeesStore: this.props.employeesStore
-        });
+function ViewEmp(props) {
+    const { selectedId, employeesStore } = props;
+    const chackStore = (key) => {
+        if(employeesStore.length == 0) {
+            return ''
+        } else {
+            return employeesStore[selectedId][key]
+        }
     };
-    render() {
-        const { selectedId, employeesStore } = this.props;
-        const employeesStoreElement = employeesStore[selectedId];
-        return (
-            <Container>
-                <Row>
-                    <Col><p><strong>Фамилия</strong></p></Col>
-                    <Col><p>{employeesStoreElement.surname}</p></Col>
-                </Row>
-                <Row>
-                    <Col><p><strong>Имя</strong></p></Col>
-                    <Col><p>{employeesStoreElement.firstName}</p></Col>
-                </Row>
-                <Row>
-                    <Col><p><strong>Отчество</strong></p></Col>
-                    <Col><p>{employeesStoreElement.lastName}</p></Col>
-                </Row>
-                <Row>
-                    <Col><p><strong>Дата рождения</strong></p></Col>
-                    <Col><p>{Moment(employeesStoreElement.birthday).format("DD.MM.YYYY")}</p></Col>
-                </Row>
-                <Row>
-                    <Col><p><strong>Табельный номер</strong></p></Col>
-                    <Col><p>{employeesStoreElement.serviceNumber}</p></Col>
-                </Row>
-                <Row>
-                    <Col><p><strong>Должность</strong></p></Col>
-                    <Col><p>{employeesStoreElement.positionEmp}</p></Col>
-                </Row>
-                <Row>
-                    <Col><p><strong>Подразделение</strong></p></Col>
-                    <Col><p>{employeesStoreElement.division}</p></Col>
-                </Row>
-                <Row>
-                    <Col>
+    return (
+        <Container>
+            {console.log(Moment(chackStore('')).format("DD.MM.YYYY"))}
+            <Row>
+                <Col><p><strong>Фамилия</strong></p></Col>
+                <Col><p>{chackStore('surname')}</p></Col>
+            </Row>
+            <Row>
+                <Col><p><strong>Имя</strong></p></Col>
+                <Col><p>{chackStore('firstName')}</p></Col>
+            </Row>
+            <Row>
+                <Col><p><strong>Отчество</strong></p></Col>
+                <Col><p>{chackStore('lastName')}</p></Col>
+            </Row>
+            <Row>
+                <Col><p><strong>Дата рождения</strong></p></Col>
+                <Col><p>{
+                    chackStore('birthday') === '' ? '' : Moment(chackStore('birthday')).format("DD.MM.YYYY")
+                }</p></Col>
+            </Row>
+            <Row>
+                <Col><p><strong>Табельный номер</strong></p></Col>
+                <Col><p>{chackStore('serviceNumber')}</p></Col>
+            </Row>
+            <Row>
+                <Col><p><strong>Должность</strong></p></Col>
+                <Col><p>{chackStore('positionEmp')}</p></Col>
+            </Row>
+            <Row>
+                <Col><p><strong>Подразделение</strong></p></Col>
+                <Col><p>{chackStore('division')}</p></Col>
+            </Row>
+            <Row>
+                <Col>
 
-                    </Col>
-                    <Col>
-                        <Row>
-                            <Col>
-                                <Button variant='secondary' onClick={(e) => this.props.getTargetButton('change')}>
-                                    Изменить
-                                </Button>
-                            </Col>
-                            <Col>
-                                <Button variant='danger' onClick={(e) => this.props.getTargetButton('delete')}>
-                                    Удалить
-                                </Button>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
+                </Col>
+                <Col>
+                    <Row>
+                        <Col>
+                            <Button variant='secondary'
+                                    disabled={employeesStore.length == 0}
+                                    onClick={(e) => props.getTargetButton('change')}>
+                                Изменить
+                            </Button>
+                        </Col>
+                        <Col>
+                            <Button variant='danger'
+                                    disabled={employeesStore.length == 0}
+                                    onClick={(e) => props.getTargetButton('delete')}>
+                                Удалить
+                            </Button>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 
 export default connect(
